3899ff499c408a7f85de42492cfd5ba74a7fc20b
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-04-28 14:31:18 -0400

    added models

---
+++ models/Room.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require('mongoose')
+
+const roomSchema = new Schema(
+    {
+        name: { type: String },
+        description: { type: String },
+        imageUrl: { type: String },
+        owner: { type: Schema.Types.ObjectId, ref: "User" },
+        reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
+    }
+  );
+
+module.exports = model('Room', roomSchema)
