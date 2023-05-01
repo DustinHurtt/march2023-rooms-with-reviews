4dceec4fa86e1666b86887c230816a481ee4b41a
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-05-01 12:41:29 -0400

    full CRUD on Room model

--- routes/rooms.js
+++ routes/rooms.js
@@ -55,7 +55,7 @@ router.get('/room-details/:id', (req, res, next) => {
             
             if (foundRoom.owner._id.toString() === req.session.user._id) {
                 copiedRoom._doc.isOwner = true
-                
+
                 } 
             }
             res.render('rooms/room-details.hbs', copiedRoom._doc)
@@ -74,6 +74,38 @@ router.get('/edit-room/:id', (req, res, next) => {
         .then((foundRoom) => {
             res.render('rooms/edit-room.hbs', foundRoom)
             
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+router.post("/edit-room/:id", (req, res, next) => {
+  const { name, description, imageUrl } = req.body;
+
+  Room.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      description,
+      imageUrl,
+    },
+    { new: true }
+  )
+    .then((updatedRoom) => {
+      console.log("Updated Room", updatedRoom);
+      res.redirect(`/rooms/room-details/${updatedRoom._id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get('/delete-room/:id', (req, res, next) => {
+    Room.findByIdAndDelete(req.params.id)
+        .then((deleted) => {
+            console.log("Deleted:", deleted)
+            res.redirect('/rooms/all-rooms')
         })
         .catch((err) => {
             console.log(err)
